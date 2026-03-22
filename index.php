28666c045d663b6ee71af0a53ff28de34a68b314
FISTON34 <irumvafiston47@gmail.com>
2026-03-22 20:30:09 +0200

    Add files via upload
    my codes of my project

---
+++ index.php
@@ -0,0 +1,54 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Solo traveler</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class=main>
+		<div class="navbar">
+			<div class="icon">
+				<h2 class="logo">SoTr</h2>
+			</div>
+			<div class="menu">
+				<ul>
+					<li><a href="#">HOME</a></li>
+					<li><a href="#">ABOUT</a></li>
+					<li><a href="#">SERVICE</a></li>
+					<li><a href="#">DESIGN</a></li>
+					<li><a href="#">CONTACT</a></li>
+				</ul>
+			</div>
+			<div class="search">
+				<input class="srch" type="search" name="" placeholder="Type to text">
+				<a href="#"> <button class="btn">search</button></a>
+			</div>
+			</div>
+			<div class="content">
+				<h1><span>THE 1000HILLS</span><br>Explore Rwanda</h1>
+				<p class="par">
+					discover the beauty of Nyungwe Forest National Park.<br> Relax in Karongi and hike the majestic Virunga Mountains.
+					<br>Experience vibrant culture, warm hospitality, and rich wildlife <br> safari encounters in Akagera National Park.
+				</p>
+				<button class="cn"><a href="#">JOIN US</a></button>
+		</div>
+		<div class="form">
+			<h2>Log here</h2>
+			<input type="Email" name="Email" placeholder="Enter Email Here">
+			<input type="password" placeholder="Enter password Here">
+			<button class="btnn"><a href="#">Login</a></button>
+			<p class="link">Don't have an account<br>
+				<a href="#">sign up here </a></p>
+			<p class="liw">log in with</p>
+			<div class="icon">
+				<a href="#"><ion-icon name="logo-facebook"></ion-icon></a>
+				<a href="#"><ion-icon name="logo-instagram"></ion-icon></a>
+				<a href="#"><ion-icon name="logo-TikTok"></ion-icon></a>
+				<a href="#"><ion-icon name="logo-snapchat"></ion-icon></a>
+		</div>
+		<script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
+	</div>
+
+</body>
+</html>
